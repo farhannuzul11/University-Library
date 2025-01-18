15ef00a4091352dceea1487fc00e606c1006ead9
Nuzul <farhan.nuzul@ui.ac.id>
2025-01-18 19:23:41 +0700

    Creating auth section

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import AuthForm from '@/components/AuthForm'
+import { signUpSchema } from '@/lib/validation'
+import { University } from 'lucide-react'
+import React from 'react'
+
+const page = () => (
+  <AuthForm
+    type="SIGN_UP"
+    schema={signUpSchema}
+    defaultValues={{ 
+      email: '', 
+      password: '',
+      fullName: '',
+      universityId: 0,
+      universityCard: '', 
+    }}
+    onSubmit={() => {}}
+  />
+)
+
+export default page
